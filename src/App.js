0e18612e4d2ce9e2cc16e93fc943e431c33a5e79
torch <harall09@evergreen.edu>
2019-11-03 20:16:46 -0500

    setup basic auth functionality & ability to see user's current spotify track

--- src/App.js
+++ src/App.js
@@ -1,26 +1,86 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React, { Component } from "react";
+import * as $ from "jquery";
+import { authEndpoint, clientId, redirectUri, scopes } from "./config";
+import hash from "./hash";
+import Player from "./Player";
+import logo from "./logo.svg";
+import "./App.css";
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+var SpotifyWebApi = require('spotify-web-api-js');
+var spotifyApi = new SpotifyWebApi();
+
+class App extends Component {
+  constructor() {
+    super();
+    this.state = {
+      item: {
+        album: {
+          images: [{ url: "" }]
+        },
+        name: "",
+        artists: [{ name: "" }],
+        duration_ms:0,
+      },
+      is_playing: "Paused",
+      progress_ms: 0
+    };
+
+  }
+  componentDidMount() {
+    let _token = hash.access_token;
+
+    if (_token) {
+      spotifyApi.setAccessToken(_token);
+      spotifyApi.getMyCurrentPlayingTrack((err, data) => {
+        if (err) console.error(err);
+        else if (data) this.setState({item: data.item, is_playing: data.is_playing, progress_ms: data.progress_ms})
+      })
+    }
+  }
+
+
+  getNowPlaying(){
+    console.log(this.state)
+    if (spotifyApi.getAccessToken()) {
+      spotifyApi.getMyCurrentPlayingTrack((err, data) => {
+        if (err) console.error(err);
+        else if (data) this.setState({item: data.item, is_playing: data.is_playing, progress_ms: data.progress_ms})
+      })
+    }
+  }
+
+  render() {
+
+    return (
+      <div className="App">
+        <header className="App-header">
+          <img src={logo} className="App-logo" alt="logo" />
+          {!this.state.token && (
+            <a
+              className="btn btn--loginApp-link"
+              href={`${authEndpoint}?client_id=${clientId}&redirect_uri=${redirectUri}&scope=${scopes.join(
+                "%20"
+              )}&response_type=token&show_dialog=true`}
+            >
+              Login to Spotify
+            </a>
+          )}
+          {
+            (spotifyApi.getAccessToken() && this.state.progress_ms) &&
+            <Player
+              item={this.state.item}
+              is_playing={this.state.is_playing}
+              progress_ms={this.state.progress_ms}
+            />
+          }
+          {this.state.progress_ms}
+          <button onClick={this.getNowPlaying.bind(this)}>
+            Check Now Playing
+          </button>
+        </header>
+      </div>
+    );
+  }
 }
 
 export default App;
---
+++ src/config.js
@@ -0,0 +1,10 @@
+export const authEndpoint = "https://accounts.spotify.com/authorize";
+
+// Replace with your app's client ID, redirect URI and desired scopes
+export const clientId = "7bda8a88522c49f186257bd1c9aed9b5";
+export const redirectUri = "http://localhost:3000";
+export const scopes = [
+    "user-top-read",
+    "user-read-currently-playing",
+    "user-read-playback-state",
+];
